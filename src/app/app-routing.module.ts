3cebc91ee5049d49e692e48334aa0bc0a7c2a226
Marcondes Junior <sinesiojunior77@gmail.com>
2023-10-16 17:11:44 -0300

    routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: FirstComponentComponent},
+  {path:'eventscomponent', component: EventsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
